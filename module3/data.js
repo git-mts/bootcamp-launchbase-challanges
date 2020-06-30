8cadc248a4cdd985ce7fcb5eb65cf5ce9c1e3476
Matheus Lucas Magalhaes de Souza <matheus.lms@icloud.com>
2020-06-30 10:20:02 -0400

    challenge 3-2 completed

---
+++ module3/data.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    id: 'starter',
+    logo_url: 'https://skylab.rocketseat.com.br/static/media/Starter.64c237cc.svg',
+    title: 'Starter',
+    description: '<strong>Torne-se um programador desejado</strong> no mercado com esses cursos gratuitos',
+    modules: 'Contem 5 módulos',
+    price: 'GRATUITO',
+  },
+  {
+    id: 'launchbase',
+    logo_url: 'https://skylab.rocketseat.com.br/static/media/LaunchBase.08285320.svg',
+    title: 'LaunchBase',
+    description: '<strong>Domine a programação do zero</strong> e tenha acesso as melhores oportunidades do mercado',
+    modules: 'Contem 8 módulos',
+    price: 'PAGO',
+  },
+  {
+    id: 'gostack',
+    logo_url: 'https://skylab.rocketseat.com.br/static/media/GoStack.83a178a0.svg',
+    title: 'GoStack',
+    description: '<strong>Treinamento imersivo</strong> nas tecnologias mais modernas de desenvolvimento web e mobile.',
+    modules: 'Contem 10 módulos',
+    price: 'PAGO',
+  },
+];
---
+++ module3/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+const courses = require('./data');
+
+const server = express();
+server.use(express.static(__dirname + '/public'));
+
+server.set('view engine', 'njk');
+
+nunjucks.configure(__dirname + '/views', {
+  express: server,
+  noCache: true
+});
+
+server.get('/', (req, res) => {
+  return res.render('index');
+});
+
+server.get('/about', (req, res) => {
+  const data = {
+    name: 'Rocketseat',
+    image_url: 'https://pbs.twimg.com/profile_images/1271517147349626881/Mf1UjRa0_400x400.jpg',
+    title: 'As melhores tecnologias em programação, direto ao ponto e do jeito certo',
+    description: 'Imagine você dominando as mesmas tecnologias adotadas pelos melhores times do mundo, construindo aplicações de alta performance e se destacando entre os maiores programadores.',
+    technologies: [
+      { name: 'NodeJS', url: 'https://nodejs.org/' },
+      { name: 'ReactJS', url: 'https://pt-br.reactjs.org/' },
+      { name: 'React Native', url: 'https://reactnative.dev/' }
+    ]
+  };
+
+  return res.render('about', { data });
+});
+
+server.get('/contents', (req, res) => {
+  return res.render('contents', { courses });
+});
+
+server.use((req, res) => {
+  return res.status(404).render("not-found");
+});
+
+server.listen(3001, () => {
+  console.log('Server is Running - Challenge 3-1');
+});
